c8c43b933ba31bbef064fbb5674e3316535fc227
BreElle Wells <breellejordyn@gmail.com>
2023-08-22 13:34:15 -0600

    convert and manipulate lists and strings

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    # if length of list is 2+, change the last item to "and item"
+    if len(items) > 1:
+        items[-1] = "and " + items[-1]
+
+    # if length of list is 3+, convert to string and add commas
+    if len(items) > 2:
+        return ", ".join(items)
+
+    # if length is not greater than 1, convert to string
+    else:
+        return " ".join(items)
